11217fa6d8f4b7b41d8f60fa58dc763290bd4245
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-04-12 11:49:35 +0900

    판매중 목록 조회 서비스 구현

--- src/main/java/multi/fclass/iMint/mypage/service/IMypageService.java
+++ src/main/java/multi/fclass/iMint/mypage/service/IMypageService.java
@@ -3,12 +3,16 @@ package multi.fclass.iMint.mypage.service;
 import java.util.List;
 
 import multi.fclass.iMint.mypage.dto.CompleteListDTO;
+import multi.fclass.iMint.mypage.dto.SellingListDTO;
 
 /**
  * @author GhostFairy
  *
  */
 public interface IMypageService {
+
+	// 판매중 목록 조회 서비스
+	public List<SellingListDTO> getSellingList(String myId, int pageNumber, int numberOfItems);
 
 	// 거래완료 목록 조회 서비스
 	public List<CompleteListDTO> getCompleteList(String myId, int pageNumber, int numberOfItems);
