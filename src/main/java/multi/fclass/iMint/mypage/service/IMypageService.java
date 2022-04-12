bf4a38da6915e450cd07ab8c74beb7b8da2a40c0
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-04-12 17:12:54 +0900

    관심 목록 조회 서비스 구현
    현재 예약중(구매자) 목록은 제외하고 관심 목록만 출력 가능

--- src/main/java/multi/fclass/iMint/mypage/service/IMypageService.java
+++ src/main/java/multi/fclass/iMint/mypage/service/IMypageService.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import multi.fclass.iMint.mypage.dto.CompleteListDTO;
 import multi.fclass.iMint.mypage.dto.SellingListDTO;
+import multi.fclass.iMint.mypage.dto.WishListDTO;
 
 /**
  * @author GhostFairy
  *
  */
 public interface IMypageService {
+
+	// 관심 목록 조회 서비스
+	public List<WishListDTO> getWishList(String myId, int pageNumber, int numberOfItems);
 
 	// 판매중 목록 조회 서비스
 	public List<SellingListDTO> getSellingList(String myId, int pageNumber, int numberOfItems);
