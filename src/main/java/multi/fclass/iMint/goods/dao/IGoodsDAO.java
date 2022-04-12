5942c09372c47d3649bc6e3f73126560dce7475e
Seongil, Yoon <dbs3346@gmail.com>
2022-04-12 13:27:12 +0900

    메인페이지 무한스크롤 완

--- src/main/java/multi/fclass/iMint/goods/dao/IGoodsDAO.java
+++ src/main/java/multi/fclass/iMint/goods/dao/IGoodsDAO.java
@@ -8,9 +8,18 @@ import org.apache.ibatis.annotations.Param;
 import multi.fclass.iMint.goods.dto.GoodsDTO;
 import multi.fclass.iMint.goods.dto.GoodsImagesDTO;
 
+/**
+ * @author Seongil, Yoon
+ *
+ */
 @Mapper
 public interface IGoodsDAO {
 	//비동기 무한스크롤
 	public List<GoodsDTO> goodsList(@Param("lastBoard") int lastBoard);
-	public GoodsImagesDTO goodsThumbnail(@Param("goods_id") int goods_id);
+	public GoodsImagesDTO goodsThumbnail(@Param("goods_id") int goodsId);
+	
+	//상품상세
+	public GoodsDTO goods(@Param("goods_id") int goodsId);
+	public List<GoodsImagesDTO> goodsImages(@Param("goods_id") int goodsId);
+	
 }
---
+++ src/main/resources/static/js/goods-detail.js
@@ -0,0 +1,3 @@
+function goodsImageReady(goodsImage){
+	console.log(goodsImage)
+}
