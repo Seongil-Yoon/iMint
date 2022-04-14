07c0fa60a3c1ddd101eab22cb0c5c280ede841c5
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-04-14 16:32:05 +0900

    관심목록 관련 서비스 구현
    상품별 관심등록수, 사용자의 상품관심등록여부 확인, 관심등록, 관심해제 서비스 작성
    RestController 작성 예정

--- src/main/java/multi/fclass/iMint/wishlist/service/IWishlistService.java
+++ src/main/java/multi/fclass/iMint/wishlist/service/IWishlistService.java
@@ -1,5 +1,17 @@
 package multi.fclass.iMint.wishlist.service;
 
+/**
+ * @author GhostFairy
+ *
+ */
 public interface IWishlistService {
+
+	public int countWishlist(int goodsId);
+	
+	public boolean checkWishlist(String myId, int goodsId);
+
+	public int addWishlist(String myId, int goodsId);
+
+	public int removeWishlist(String myId, int goodsId);
 
 }
