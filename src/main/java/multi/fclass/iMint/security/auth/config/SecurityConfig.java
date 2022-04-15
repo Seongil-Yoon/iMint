46c01c7248ed3ad22881f7ab7cb1d3698a383fba
it_st it <overult01@gmail.com>
2022-04-15 13:44:22 +0900

    securityConfig

--- src/main/java/multi/fclass/iMint/security/auth/config/SecurityConfig.java
+++ src/main/java/multi/fclass/iMint/security/auth/config/SecurityConfig.java
@@ -5,8 +5,13 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 import lombok.AllArgsConstructor;
+import multi.fclass.iMint.security.jwt.JwtAuthenticationEntryPoint;
+import multi.fclass.iMint.security.jwt.JwtSecurityConfig;
+import multi.fclass.iMint.security.jwt.TokenProvider;
 import multi.fclass.iMint.security.service.CustomOAuth2UserService;
 
 @AllArgsConstructor
@@ -14,14 +19,28 @@ import multi.fclass.iMint.security.service.CustomOAuth2UserService;
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화 
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
-    private final CustomOAuth2UserService customOAuth2UserService;
 	
-//    private final WebAccessDeniedHandler webAccessDeniedHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    // jwt
+//    private final TokenProvider tokenProvider;
+//    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable(); // csrf: 페이지 위변조 방지. 유니크 키값 토큰넣어줌. form태그 안에 넣어주는 메서드 찾기. 비동기할 때도 참고.
+		http.csrf().disable() // csrf: 페이지 위변조 방지. 유니크 키값 토큰넣어줌. form태그 안에 넣어주는 메서드 찾기. 비동기할 때도 참고.
+			.exceptionHandling()
+//			.authenticationEntryPoint(jwtAuthenticationEntryPoint) // jwt
+			.accessDeniedPage("/err/denied-page"); // 접근 불가 페이지 (나중에 핸들러로 바꿀 수도)
+		
+		// h2-console 설정 추가
+		http.headers()
+			.frameOptions()
+			.sameOrigin();
+		
+		// 세션 해제(jwt로 대체)
+		http.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 		http.authorizeRequests()
 			.antMatchers("/", "/main", "/login/**", "/static/**").permitAll() // 비로그인시 첫화면, 둘러보기만 허용
@@ -29,13 +48,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers("/register/**").access("hasRole('ROLE_uncerti_GAURD') or hasRole('ROLE_uncerti_CHILD') or hasRole('ROLE_ADMIN')")
 			
 			.antMatchers("/mypage/**").access("hasRole('ROLE_GAURD') or hasRole('ROLE_CHILD')") // or hasRole('ROLE_ADMIN')
-			.antMatchers("/chat/**").access("hasRole('ROLE_GAURD') or hasRole('ROLE_CHILD')") //  or hasRole('ROLE_ADMIN')
 			.antMatchers("/goods/**").access("hasRole('ROLE_GAURD') or hasRole('ROLE_CHILD')") //  or hasRole('ROLE_ADMIN')
+			.antMatchers("/chat/**").access("hasRole('ROLE_GAURD') or hasRole('ROLE_CHILD')") //  or hasRole('ROLE_ADMIN') // 뷰 
+			.antMatchers("/chatting/**").access("hasRole('ROLE_GAURD') or hasRole('ROLE_CHILD')") // 웹소켓 
 			
 			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") // 로그인한 admin만 들어올 수 있다.
             
 			.anyRequest().denyAll(); // // 허용가능 외 나머지는 인증 받아야 접근 가능(접근불가) 
 
+//		http.apply(new JwtSecurityConfig(tokenProvider));
 		
 //		http.formLogin()
 //			.loginPage("/login") // 권한이 없는 페이지로 가려고 하면 login페이지로 보내기
@@ -54,8 +75,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.userInfoEndpoint() //oauth2Login 성공 이후의 설정을 시작
 			.userService(customOAuth2UserService); // 들어가야 할 타입이 OAuth2UserService
 		
-		http.exceptionHandling().accessDeniedPage("/err/denied-page");
-//		http.exceptionHandling()
-//			.accessDeniedHandler(webAccessDeniedHandler); // 접근 불가 페이지일 때 처리할 함수 
 	}
 }
