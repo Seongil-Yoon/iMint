09c5e0e9bd6d8de2effaddb91fcb03148120f7cd
it_st it <overult01@gmail.com>
2022-04-18 15:17:23 +0900

    보호자 인증번호

---
+++ src/main/java/multi/fclass/iMint/security/GenerateCertCharacter.java
@@ -0,0 +1,30 @@
+package multi.fclass.iMint.security;
+import java.util.Random;
+
+public class GenerateCertCharacter{ // 보호자 8자리 영문+숫자 인증 번호 생성
+	
+    private int certCharLength = 8;
+    private final char[] characterTable = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+                                            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
+                                            'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+    
+    public String excuteGenerate() {
+        Random random = new Random(System.currentTimeMillis());
+        int tablelength = characterTable.length;
+        StringBuffer buf = new StringBuffer();
+        
+        for(int i = 0; i < certCharLength; i++) {
+            buf.append(characterTable[random.nextInt(tablelength)]);
+        }
+        
+        return buf.toString();
+    }
+
+    public int getCertCharLength() {
+        return certCharLength;
+    }
+
+    public void setCertCharLength(int certCharLength) {
+        this.certCharLength = certCharLength;
+    }
+}
