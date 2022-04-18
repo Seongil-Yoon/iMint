bc4875e3be4542dec58b7dda15749a7641d3848c
it_st it <overult01@gmail.com>
2022-04-18 23:02:44 +0900

    회원정보수정(프로필 사진 외), 탈퇴

--- src/main/java/multi/fclass/iMint/member/controller/MemberCotroller.java
+++ src/main/java/multi/fclass/iMint/member/controller/MemberCotroller.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import multi.fclass.iMint.member.dao.IMemberDAO;
