3f707983d6b69a0cb6e477ac6e805e18ebc150ad
hailey-hy <lillyine.hy@gmail.com>
2022-04-19 11:39:29 +0900

    마이페이지 수정 요청사항 반영 중 (mbRole별 다른 페이지 매핑, form action 속성 삽입)

--- src/main/java/multi/fclass/iMint/mypage/controller/MypageCotroller.java
+++ src/main/java/multi/fclass/iMint/mypage/controller/MypageCotroller.java
@@ -1,9 +1,18 @@
 package multi.fclass.iMint.mypage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import multi.fclass.iMint.member.dao.IMemberDAO;
+import multi.fclass.iMint.security.dao.IUserDAO;
+import multi.fclass.iMint.security.dto.Role;
+import multi.fclass.iMint.security.dto.User;
+import multi.fclass.iMint.security.parsing.mbid.ParseMbId;
 
 
 /**
@@ -14,6 +23,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MypageCotroller {
+	
+	@Autowired
+	User user;
+	
+	@Autowired
+	IMemberDAO memberDAO;
+	
+	@Autowired
+	IUserDAO userDAO;
+	
+	@Autowired
+	ParseMbId parseMbId;
+	
 	@GetMapping("mypage")
 	public String index() {
 		return "member/baby-mypage/baby-main";
@@ -24,14 +46,42 @@ public class MypageCotroller {
 		return "member/guard-mypage/guard-location";
 	}
 	
+//	@GetMapping("mypage/mylist")
+//	public String indexMylist() {
+//		return "member/baby-mypage/baby-myList";
+//	}
+	
 	@GetMapping("mypage/mylist")
-	public String indexMylist() {
-		return "member/baby-mypage/baby-myList";
+	public ModelAndView indexMylist(Authentication auth) {
+		ModelAndView mv = new ModelAndView();
+		
+		String mbId = parseMbId.parseMbId(auth);
+		User user = parseMbId.getUserMbId(mbId);
+		
+		if(user.getMbRole() == Role.GUARD) {
+			mv.setViewName("member/baby-mypage/guard-myList"); 
+		}
+		else if(user.getMbRole() == Role.CHILD) {
+			mv.setViewName("member/baby-mypage/baby-myList");
+		}
+		return mv;
 	}
 	
 	@GetMapping("mypage/block")
-	public String indexBlocklist() {
-		return "member/baby-mypage/baby-blocklist";
+	public ModelAndView indexBlocklist(Authentication auth) {
+		ModelAndView mv = new ModelAndView();
+		
+		String mbId = parseMbId.parseMbId(auth);
+		User user = parseMbId.getUserMbId(mbId);
+		
+		if(user.getMbRole() == Role.GUARD) {
+			mv.setViewName("member/baby-mypage/guard-blocklist"); 
+		}
+		else if(user.getMbRole() == Role.CHILD) {
+			mv.setViewName("member/baby-mypage/baby-blocklist");
+		}
+		
+		return mv;
 	}
 	
 //	@GetMapping("mypage/edit")
