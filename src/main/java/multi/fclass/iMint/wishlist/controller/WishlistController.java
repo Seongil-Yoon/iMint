587abe0ee99c346b5207e9929cd4d5daf49d73fe
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-04-27 14:52:14 +0900

    관심목록 관련 HTTP 메소드 수정
    데이터를 가져오는 동작은 GET, 그 외의 동작은 POST로 할당했습니다

--- src/main/java/multi/fclass/iMint/wishlist/controller/WishlistController.java
+++ src/main/java/multi/fclass/iMint/wishlist/controller/WishlistController.java
@@ -2,7 +2,8 @@ package multi.fclass.iMint.wishlist.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nimbusds.jose.shaded.json.JSONObject;
@@ -19,7 +20,7 @@ public class WishlistController {
 	@Autowired
 	ParseMbId parseService;
 
-	@RequestMapping("/wishlist/count")
+	@GetMapping("/wishlist/count")
 	public String countWishes(Integer goodsId) {
 		JSONObject out = new JSONObject();
 
@@ -28,7 +29,7 @@ public class WishlistController {
 		return out.toJSONString();
 	}
 
-	@RequestMapping("/wishlist/check")
+	@GetMapping("/wishlist/check")
 	public String checkWish(Authentication auth, Integer goodsId) {
 		String myId = parseService.parseMbId(auth);
 		JSONObject out = new JSONObject();
@@ -38,7 +39,7 @@ public class WishlistController {
 		return out.toJSONString();
 	}
 
-	@RequestMapping("/wishlist/add")
+	@PostMapping("/wishlist/add")
 	public String addWish(Authentication auth, Integer goodsId) {
 		String myId = parseService.parseMbId(auth);
 		JSONObject out = new JSONObject();
@@ -49,7 +50,7 @@ public class WishlistController {
 		return out.toJSONString();
 	}
 
-	@RequestMapping("/wishlist/remove")
+	@PostMapping("/wishlist/remove")
 	public String removeWish(Authentication auth, Integer goodsId) {
 		String myId = parseService.parseMbId(auth);
 		JSONObject out = new JSONObject();
