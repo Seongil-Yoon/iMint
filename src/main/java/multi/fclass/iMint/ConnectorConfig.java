e795d81f392941a57308022745946953a7019dce
Seongil, Yoon <dbs3346@gmail.com>
2022-04-28 01:59:57 +0900

    https연결설정v1.1.1

--- src/main/java/multi/fclass/iMint/ConnectorConfig.java
+++ src/main/java/multi/fclass/iMint/ConnectorConfig.java
@@ -34,8 +34,9 @@ public class ConnectorConfig {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
 		connector.setSecure(false);
-		connector.setPort(80);
-		connector.setRedirectPort(443);
+		connector.setPort(8080);
+		connector.setRedirectPort(8443);
 		return connector;
 	}
+
 }
