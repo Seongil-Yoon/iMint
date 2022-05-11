74811f74a13de885f67e9ced5ad333c1a75fadbe
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-05-11 23:28:07 +0900

    평가 기능 구현

--- src/main/java/multi/fclass/iMint/transaction/service/TransactionServiceImpl.java
+++ src/main/java/multi/fclass/iMint/transaction/service/TransactionServiceImpl.java
@@ -20,6 +20,21 @@ public class TransactionServiceImpl implements ITransactionService {
 	ITransactionDAO trxDAO;
 
 	@Override
+	public int getTransaction(String myId, int goodsId) {
+		TransactionCheckDTO trxCheckDTO = trxDAO.checkTransaction(goodsId);
+
+		if (trxCheckDTO == null || trxCheckDTO.getId() == null) {
+			return -1;
+		} else {
+			if (trxCheckDTO.getSellerId().equals(myId) || trxCheckDTO.getBuyerId().equals(myId)) {
+				return trxCheckDTO.getId();
+			} else {
+				return -1;
+			}
+		}
+	}
+
+	@Override
 	public String checkTransaction(String myId, int goodsId) {
 		return checkTransaction(myId, null, goodsId);
 	}
@@ -65,32 +80,23 @@ public class TransactionServiceImpl implements ITransactionService {
 				}
 			}
 		} else {
-			if (trxCheckDTO.getBuyerId() == null) {
-				if (trxCheckDTO.getSellerId().equals(myId)) {
-					// 거래완료, 구매자 지정안함(판매자)
-					return "comp?_seller";
+			if (trxCheckDTO.getSellerId().equals(myId)) {
+				if (opponentId != null && trxCheckDTO.getBuyerId().equals(opponentId)) {
+					// 거래완료(판매자/구매자)
+					return "comp!_seller_match";
 				} else {
-					// 거래완료, 구매자 지정안함(판매자/구매자 아님)
-					return "comp_other";
+					// 거래완료(판매자)
+					return "comp!_seller";
 				}
+			} else if (trxCheckDTO.getBuyerId().equals(myId)) {
+				// 거래완료(구매자)
+				return "comp!_buyer";
 			} else {
-				if (trxCheckDTO.getSellerId().equals(myId)) {
-					if (opponentId != null && trxCheckDTO.getBuyerId().equals(opponentId)) {
-						// 거래완료(판매자/구매자)
-						return "comp!_seller_match";
-					} else {
-						// 거래완료(판매자)
-						return "comp!_seller";
-					}
-				} else if (trxCheckDTO.getBuyerId().equals(myId)) {
-					// 거래완료(구매자)
-					return "comp!_buyer";
-				} else {
-					// 거래완료(판매자/구매자 아님)
-					return "comp_other";
-				}
+				// 거래완료(판매자/구매자 아님)
+				return "comp_other";
 			}
 		}
+
 	}
 
 	@Override
