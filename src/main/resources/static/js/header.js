aa9054a0c2d5ac19edd67beaba02cd7b31476ceb
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-05-12 12:00:04 +0900

    툴팁을 자체 CSS에서 부트스트랩 기능으로 변경

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,25 +1,3 @@
-
-function resizeArrowBox(){
-	//음성
-	const voiceBtn = document.querySelector('#search-voice');
-	
-    const voiceBtnLeft = voiceBtn.getBoundingClientRect().left;
-    
-    
-    const voiceArrowBox = document.querySelector('#arrow-voice');
-    
-    voiceArrowBox.style.marginLeft = voiceBtnLeft - 38 + 'px';
-    
-    //텍스트
-    const textBtn = document.querySelector('#search-text');
-	
-    const textBtnLeft = textBtn.getBoundingClientRect().left;
-    
-    
-    const textArrowBox = document.querySelector('#arrow-text');
-    
-    textArrowBox.style.marginLeft = voiceBtnLeft - 5 + 'px';
-    }
-
-window.addEventListener('resize', resizeArrowBox);
-window.addEventListener('load', resizeArrowBox);
+new bootstrap.Tooltip(document.body, {
+    selector: '[data-bs-toggle="tooltip"]',
+});
