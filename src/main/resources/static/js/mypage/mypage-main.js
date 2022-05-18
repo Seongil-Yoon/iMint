7113157fa37a1bae2f7d72f168268427d6ef7702
hailey-hy <lillyine.hy@gmail.com>
2022-05-18 15:21:24 +0900

    레벨 구간마다 색상 변경

--- src/main/resources/static/js/mypage/mypage-main.js
+++ src/main/resources/static/js/mypage/mypage-main.js
@@ -1,14 +1,33 @@
 //레벨 변환
 
-var level_default = parseInt(document.getElementById('leveling').innerText);
+var level_default = parseInt(document.getElementById('leveling').innerText) - 15;
 const levelbtn = document.getElementById('leveling');
+
+console.log(levelbtn);
 
 if(level_default <= 0){
 	var level = 0;
 	} else if(level_default <= 100){
-		level = level_default / 10;
+		level = level_default / 10
+		if(level < 3){
+			levelbtn.style.backgroundColor = 'rgb(192, 192, 192)';
+			
+		} else if(level < 5){
+			levelbtn.style.backgroundColor = 'rgba(202,255,133,1)';
+	
+		} else if(level < 7){
+			levelbtn.style.backgroundColor = 'rgba(240,235,113,1)';
+			
+		} else if(level < 9){
+			levelbtn.style.backgroundColor = 'rgba(166,193,238,1)';
+			
+		} else if(level < 10){
+			levelbtn.style.backgroundColor = 'rgba(251,194,235,1)';
+		}
+		
 	} else {
 		level = "MAX";
+		levelbtn.style.background = 'linear-gradient(90deg, rgba(251,194,235,1) 0%, rgba(251,194,235,1) 0%, rgba(166,193,238,1) 100%)';
 	}
 
 levelbtn.innerText = "레벨 " + level;
