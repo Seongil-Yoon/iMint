ee042e6254499e001dca00680364ba79a02e40c4
Seongil, Yoon <dbs3346@gmail.com>
2022-05-21 04:15:38 +0900

    거래메일알림 정보추가
    상품정보 추가

---
+++ src/test/java/multi/fclass/iMint/notification/service/NotificationServiceImplTest.java
@@ -0,0 +1,34 @@
+package multi.fclass.iMint.notification.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NotificationServiceImplTest {
+
+	@Test
+	void testNotifyReservation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotifyCancelReservation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotifyTransaction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotifyNewMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotifyNewGoods() {
+		fail("Not yet implemented");
+	}
+
+}
