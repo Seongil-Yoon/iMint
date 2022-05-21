7b5d4add51ae05b75f5d4cf6e2de59cd93f8c357
it_st it <overult01@gmail.com>
2022-05-22 01:11:07 +0900

    mybatis 연결테스트

---
+++ src/test/java/DataSourceTest.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import javax.sql.DataSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+// mybatis 연결테스트 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+	locations ={"file:src/main/resorces/mybatis/*.xml"})
+public class DataSourceTest {
+
+	private DataSource ds;
+
+	@Test
+	public void testConection()throws Exception{
+		
+		try(Connection con = ds.getConnection()){
+			System.out.println(con);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
