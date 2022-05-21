9b94a18ba17a5e1974349976d94121fe9b2c5684
it_st it <overult01@gmail.com>
2022-05-21 21:30:29 +0900

    롬복적용으로 리팩토링

--- src/main/java/multi/fclass/iMint/block/dto/BlockDTO.java
+++ src/main/java/multi/fclass/iMint/block/dto/BlockDTO.java
@@ -4,11 +4,20 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author Junming, Yang
  *
  */
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Component
 public class BlockDTO {
 
@@ -26,58 +35,6 @@ public class BlockDTO {
 
 	// 차단 취소 여부
 	private Boolean blockIsdelete;
-	
-
-	public BlockDTO() {
-	}
-
-	public BlockDTO(Integer blockId, String mbId, String mbId2, LocalDateTime blockDate, Boolean blockIsdelete) {
-		this.blockId = blockId;
-		this.mbId = mbId;
-		this.mbId2 = mbId2;
-		this.blockDate = blockDate;
-		this.blockIsdelete = blockIsdelete;
-	}
-
-	public Integer getBlockId() {
-		return blockId;
-	}
-
-	public void setBlockId(Integer blockId) {
-		this.blockId = blockId;
-	}
-
-	public String getMbId() {
-		return mbId;
-	}
-
-	public void setMbId(String mbId) {
-		this.mbId = mbId;
-	}
-
-	public String getMbId2() {
-		return mbId2;
-	}
-
-	public void setMbId2(String mbId2) {
-		this.mbId2 = mbId2;
-	}
-
-	public LocalDateTime getBlockDate() {
-		return blockDate;
-	}
-
-	public void setBlockDate(LocalDateTime blockDate) {
-		this.blockDate = blockDate;
-	}
-
-	public Boolean getBlockIsdelete() {
-		return blockIsdelete;
-	}
-
-	public void setBlockIsdelete(Boolean blockIsdelete) {
-		this.blockIsdelete = blockIsdelete;
-	}
 
 	@Override
 	public String toString() {
